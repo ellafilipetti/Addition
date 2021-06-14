a2b1d79a01d67ea1ce33ce8e6634e690a94c56b6
ellafilipetti <72880855+ellafilipetti@users.noreply.github.com>
2021-06-14 10:33:35 -0400

    14 juin, commit Exemple 2

---
+++ ella_juin/src/ella_juin/Exemple2.java
@@ -0,0 +1,16 @@
+package ella_juin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Exemple2 extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
