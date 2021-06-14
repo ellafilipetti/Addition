86651b27e9f332bd9dc0e50a0734d6fae8c0d10f
ellafilipetti <72880855+ellafilipetti@users.noreply.github.com>
2021-06-14 10:33:53 -0400

    14 juin, commit Exemple 2

--- ella_juin/src/ella_juin/Exemple2.java
+++ ella_juin/src/ella_juin/Exemple2.java
@@ -1,16 +1,35 @@
+//Date : 14 juin
+//Auteur: efilipetti
+
 package ella_juin;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Exemple2 extends Application {
+public class Exemple2 {
 
-	@Override
-	public void start(Stage primaryStage) {
+
+	public static void main(String[] args)  throws IOException,FileNotFoundException
+	{
+		FileReader ficALire;
+		BufferedReader entree;
+		char c;
 		
+			ficALire=new FileReader("monFichier.txt");
+			entree = new BufferedReader(ficALire);
+			c=(char) entree.read();
+			System.out.print(c);
+			c=(char) entree.read();
+			System.out.print(c);
+			c=(char) entree.read();
+			System.out.print(c);
+			entree.close();
+			ficALire.close();
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
 }
